7d70183f92b48cdbc1fc7322c9938c9008b4f7ee
satorionK <businekami@gmail.com>
2019-02-11 10:49:05 +0900

    追加します

--- MainActivity.java
+++ MainActivity.java
@@ -80,25 +80,227 @@ private class ListItemClickListener implements AdapterView.OnItemClickListener{
         Map<String, Object> menu = new HashMap<>();
         menu.put("name","博麗霊夢");
         menu.put("ability","空を飛ぶ");
-        menu.put("desc","夢想封印");
+        menu.put("desc","人間");
         mList.add(menu);
 
         menu = new HashMap<>();
         menu.put("name","霧雨魔理沙");
         menu.put("ability","魔法を使う");
-        menu.put("desc","マスタースパーク");
+        menu.put("desc","人間");
+        mList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name","十六夜咲夜");
+        menu.put("ability","時間を操る");
+        menu.put("desc","人間");
+        mList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name","魂魄妖夢");
+        menu.put("ability","剣術を扱う");
+        menu.put("desc","半人半霊");
+        mList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name","アリス・マーガトロイド");
+        menu.put("ability","人形を操る");
+        menu.put("desc","人間");
+        mList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name","射命丸文");
+        menu.put("ability","風を操る");
+        menu.put("desc","天狗");
+        mList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name","東風谷早苗");
+        menu.put("ability","奇跡を起こす");
+        menu.put("desc","人間");
         mList.add(menu);
 
         return mList;
-
-
     }
 
+    private List<Map<String, Object>> createKouma(){
+        List<Map<String,Object>> mList = new ArrayList<>();
 
+        Map<String, Object> menu = new HashMap<>();
+        menu.put("name","ルーミア");
+        menu.put("ability","闇を操る");
+        menu.put("desc","妖精");
+        mList.add(menu);
 
+        menu = new HashMap<>();
+        menu.put("name","大妖精");
+        menu.put("ability","不明");
+        menu.put("desc","妖精");
+        mList.add(menu);
 
+        menu = new HashMap<>();
+        menu.put("name","チルノ");
+        menu.put("ability","冷気・氷を操る");
+        menu.put("desc","妖精");
+        mList.add(menu);
 
+        menu = new HashMap<>();
+        menu.put("name","紅美鈴");
+        menu.put("ability","気を使う");
+        menu.put("desc","妖怪");
+        mList.add(menu);
 
+        menu = new HashMap<>();
+        menu.put("name","十六夜咲夜");
+        menu.put("ability","時間を操る");
+        menu.put("desc","人間");
+        mList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name","小悪魔");
+        menu.put("ability","不明");
+        menu.put("desc","悪魔");
+        mList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name","パチュリー・ノーレッジ");
+        menu.put("ability","火水木金土日月を操る");
+        menu.put("desc","魔法使い");
+        mList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name","レミリア・スカーレット");
+        menu.put("ability","運命を操る");
+        menu.put("desc","吸血鬼");
+        mList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name","レミリア・スカーレット");
+        menu.put("ability","運命を操る");
+        menu.put("desc","吸血鬼");
+        mList.add(menu);
+
+        return mList;
+    }
+
+    private List<Map<String, Object>> createYouyoumu(){
+        List<Map<String,Object>> mList = new ArrayList<>();
+
+        Map<String, Object> menu = new HashMap<>();
+        menu.put("name","レティ・ホワイトロック");
+        menu.put("ability","寒気を操る");
+        menu.put("desc","妖精");
+        mList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name","橙");
+        menu.put("ability","妖術を扱う");
+        menu.put("desc","妖獣");
+        mList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name","アリス・マーガトロイド");
+        menu.put("ability","人形を操る");
+        menu.put("desc","人間");
+        mList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name","リリーホワイト");
+        menu.put("ability","春が来たことを伝える");
+        menu.put("desc","妖精");
+        mList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name","ルナサ・プリズムリバー");
+        menu.put("ability","鬱の音を演奏する");
+        menu.put("desc","妖精");
+        mList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name","メルラン・プリズムリバー");
+        menu.put("ability","躁の音を演奏する");
+        menu.put("desc","妖精");
+        mList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name","リリカ・プリズムリバー");
+        menu.put("ability","幻想の音を演奏する");
+        menu.put("desc","妖精");
+        mList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name","魂魄妖夢");
+        menu.put("ability","剣術を扱う");
+        menu.put("desc","半人半霊");
+        mList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name","西行寺幽々子");
+        menu.put("ability","死を操る");
+        menu.put("desc","亡霊");
+        mList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name","八雲藍");
+        menu.put("ability","式神を操る");
+        menu.put("desc","妖獣");
+        mList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name","八雲紫");
+        menu.put("ability","境界を操る");
+        menu.put("desc","妖怪");
+        mList.add(menu);
+
+        return mList;
+    }
+
+    private List<Map<String, Object>> createEiyasyou(){
+        List<Map<String,Object>> mList = new ArrayList<>();
+
+        Map<String, Object> menu = new HashMap<>();
+        menu.put("name","リグル･ナイトバグ");
+        menu.put("ability","蟲を操る");
+        menu.put("desc","妖精");
+        mList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name","ミスティア・ローレライ");
+        menu.put("ability","歌で人を惑わす");
+        menu.put("desc","妖精");
+        mList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name","因幡てゐ");
+        menu.put("ability","人間を幸運にする");
+        menu.put("desc","妖獣");
+        mList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name","鈴仙・優曇華院・イナバ");
+        menu.put("ability","狂気を操る程度");
+        menu.put("desc","月人");
+        mList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name","八意永琳");
+        menu.put("ability","あらゆる薬を作る");
+        menu.put("desc","月人");
+        mList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name","蓬莱山輝夜");
+        menu.put("ability","永遠と須臾を操る");
+        menu.put("desc","月人");
+        mList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name","藤原妹紅");
+        menu.put("ability","老いる事も死ぬ事も無い");
+        menu.put("desc","人間（蓬莱人）");
+        mList.add(menu);
+
+        return mList;
+    }
 
 
 
